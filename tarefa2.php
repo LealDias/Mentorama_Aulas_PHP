7edeba1c1363e8e674767cb5fa25a2a89184295c
Diego Leal <70763447+LealDias@users.noreply.github.com>
2022-06-11 13:04:31 -0300

    Commit da Tarefa 2
    Upload da tarefa 2. Detalhes no README.MD

---
+++ tarefa2.php
@@ -0,0 +1,11 @@
+<?php
+
+// Função que recebe 2 valores por parâmetro e retorne a sua multiplicação
+
+function multiplica($a, $b){
+    return $a * $b;
+}
+
+echo multiplica(2,5);
+
+// No exemplo acima o retorno será 10;
